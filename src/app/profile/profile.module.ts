ef8fd17f6bc7fb88d9e1c322ee485ba243f201da
SvenOu <ouzhijian@daoran.tv>
2019-12-24 17:23:31 +0800

    重构

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -1,11 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import { MyCenterComponent } from './component/my-center/my-center.component';
+import {NgZorroAntdMobileModule} from 'ng-zorro-antd-mobile';
 
 
 @NgModule({
   declarations: [MyCenterComponent],
   imports: [
+    /** 导入 ng-zorro-antd-mobile 模块 **/
+    NgZorroAntdMobileModule,
     CommonModule
   ]
 })
