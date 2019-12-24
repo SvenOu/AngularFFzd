348ee0095bff0e478369043b45e59e590bf69d57
SvenOu <ouzhijian@daoran.tv>
2019-12-24 18:20:10 +0800

    重构

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import { MyCenterComponent } from './component/my-center/my-center.component';
 import {NgZorroAntdMobileModule} from 'ng-zorro-antd-mobile';
+import {TranslatorModule} from 'angular-translator';
 
 
 @NgModule({
@@ -9,7 +10,14 @@ import {NgZorroAntdMobileModule} from 'ng-zorro-antd-mobile';
   imports: [
     /** 导入 ng-zorro-antd-mobile 模块 **/
     NgZorroAntdMobileModule,
-    CommonModule
+    CommonModule,
+    /** 导入 国际化 模块 **/
+    TranslatorModule.forRoot({
+      providedLanguages: ['zh'],
+      defaultLanguage: 'zh',
+      // 默认根据运行环境,自动选择语言
+      detectLanguage: true
+    })
   ]
 })
 export class ProfileModule { }
