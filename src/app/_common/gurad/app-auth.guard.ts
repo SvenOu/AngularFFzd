b40754bd2c98d240e728f56b3888e439f2ba5da7
SvenOu <ouzhijian@daoran.tv>
2019-12-24 11:25:22 +0800

    重构

--- src/app/_common/gurad/app-auth.guard.ts
+++ src/app/_common/gurad/app-auth.guard.ts
@@ -18,7 +18,10 @@ import {AppConstant} from '../bean/AppConstant';
 export class AppAuthGuard implements CanActivate{
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     console.log(state);
-    // AppConstant.typeVIPOrder todo 根据此字符串判断
+    // 如果是vip界面拦截
+    if(state.url.lastIndexOf(AppConstant.typeVIPOrder) > 0){
+      return false;
+    }
     return true;
   }
 }
